eb028388b6d200c5001a3e9ab773c8fd4f480bfa
Cory Lueninghoener <cluening@lanl.gov>
2019-04-05 13:23:58 -0600

    Fixes a typo in a variable name

--- ansiblerepo/roles/userenvironment/files/etc/profile.d/openmpimca.sh
+++ ansiblerepo/roles/userenvironment/files/etc/profile.d/openmpimca.sh
@@ -1,4 +1,4 @@
 # These are needed on our virtual cluster to keep openmpi from trying
 # to use the virtualbox interface
-export OMPI_MCA_btl_tcp_if_includ=eth1
+export OMPI_MCA_btl_tcp_if_include=eth1
 export OMPI_MCA_oob_tcp_if_include=eth1
