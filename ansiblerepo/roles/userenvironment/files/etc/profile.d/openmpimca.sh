e88f725f469942137906e4d8e37a42b72c219f94
Cory Lueninghoener <cluening@lanl.gov>
2019-04-08 08:47:41 -0600

    Adds network device selection for UCX

--- ansiblerepo/roles/userenvironment/files/etc/profile.d/openmpimca.sh
+++ ansiblerepo/roles/userenvironment/files/etc/profile.d/openmpimca.sh
@@ -2,3 +2,6 @@
 # to use the virtualbox interface
 export OMPI_MCA_btl_tcp_if_include=eth1
 export OMPI_MCA_oob_tcp_if_include=eth1
+
+# This is needed to keep UCX from trying to use the virtualbox interface
+UCX_NET_DEVICES=eth1
